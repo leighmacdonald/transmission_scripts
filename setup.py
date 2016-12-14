6777ba679853a55557da59ab017da2c89c2db4a7
Leigh MacDonald <leigh.macdonald@gmail.com>
2016-12-14 05:34:49 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 setup(
     name='Transmission Scripts',
     version='1.0',
+    requires=['transmissionrpc'],
     description='Various scripts to manage transmission',
     author='Leigh MacDonald',
     author_email='leigh.macdonald@gmail.com',
