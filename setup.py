a816ded65fab2cd11673888cf3ff3aa890cdfe68
Leigh MacDonald <leigh.macdonald@gmail.com>
2016-12-15 03:58:53 -0700

    Updated setup.py with new scripts and requirements

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 setup(
     name='Transmission Scripts',
     version='1.0',
-    requires=['transmissionrpc'],
+    requires=['transmissionrpc', 'termcolor'],
     description='Various scripts to manage transmission',
     author='Leigh MacDonald',
     author_email='leigh.macdonald@gmail.com',
     url='https://github.com/leighmacdonald/transmissionscripts',
     packages=['transmissionscripts'],
-    scripts=['scripts/ts_clean.py']
+    scripts=['scripts/ts_clean.py', 'scripts/ts_cli.py', 'scripts/ts_list.py']
 )
