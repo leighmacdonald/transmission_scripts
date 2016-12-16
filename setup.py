4d8e3762af1ed703803adc993adaa2cea0c8e836
Leigh MacDonald <leigh.macdonald@gmail.com>
2016-12-15 18:52:10 -0700

    Version 0.2 Release

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 from distutils.core import setup
 
 setup(
-    name='Transmission Scripts',
-    version='0.1',
+    name='transmissionscripts',
+    version='0.2',
     requires=['transmissionrpc', 'termcolor'],
     description='Various scripts to manage transmission',
     author='Leigh MacDonald',
@@ -11,6 +11,6 @@ setup(
     url='https://github.com/leighmacdonald/transmissionscripts',
     packages=['transmissionscripts'],
     scripts=['scripts/ts_clean.py', 'scripts/ts_cli.py', 'scripts/ts_list.py'],
-    download_url='https://github.com/leighmacdonald/transmission_scripts/tarball/0.1',
+    download_url='https://github.com/leighmacdonald/transmission_scripts/tarball/0.2',
     keywords=["torrent", "transmission", "p2p"]
 )
