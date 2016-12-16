d7420d25df5106d9f9922c367f1fc11bd9098c7f
Leigh MacDonald <leigh.macdonald@gmail.com>
2016-12-15 19:42:39 -0700

    Fix packaging error

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 from distutils.core import setup
+from os.path import dirname, join
+
+long_desc = open(join(dirname(__file__), "README.rst")).read()
 
 setup(
     name='transmissionscripts',
@@ -8,7 +11,7 @@ setup(
     description='Various scripts to manage transmission',
     author='Leigh MacDonald',
     author_email='leigh.macdonald@gmail.com',
-    long_description=open('README.rst').read(),
+    long_description=long_desc,
     url='https://github.com/leighmacdonald/transmission_scripts',
     packages=['transmissionscripts'],
     scripts=['scripts/ts_clean.py', 'scripts/ts_cli.py', 'scripts/ts_list.py'],
