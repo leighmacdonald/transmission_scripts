4e8c1e22ef6d02fdfd25c12c97620663212c703e
Leigh MacDonald <leigh.macdonald@gmail.com>
2016-12-18 05:51:52 -0700

    0.3.1 rls

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import dirname, join
 
 import sys
 
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 
 if sys.platform == "win32":
     install_requires = ['transmissionrpc', 'termcolor', 'colorama']
